b6a89194b5a272ae945ad7abc6e47dfbfa287622
anushkamishra7355 <anushkamishra7355@gmail.com>
2026-01-20 11:09:30 +0530

    learning stacks

---
+++ introToStack.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stack>
+using namespace std;
+
+int main(){
+    stack<int> s;
+    s.push(1);
+    s.push(2);
+    s.push(3);
+    s.push(4);
+    s.push(5);
+
+    cout<<s.top()<<endl;
+
+    s.pop();
+      s.pop();
+        s.pop();
+
+    if(s.empty()){
+        cout<<"stack is empty"<<endl;
+    }else{
+        cout<<"stack isnt empty";
+    }
+
+
+
+}
